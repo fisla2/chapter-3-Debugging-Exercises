4572be6e7c9e4527eb6affc0de5c9162b9ec8e2d
fisla2 <fisla1146362@mail.wcccd.edu>
2025-04-01 11:27:20 -0400

    Create DebugThree4.java

---
+++ DebugThree4.java
@@ -0,0 +1,8 @@
+public class FixDebugThree4 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int index = 4;  // Set to a valid index
+        
+        System.out.println("Element at index " + index + ": " + numbers[index]);
+    }
+}
