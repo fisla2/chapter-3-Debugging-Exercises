63caf4905778ba6403636e9b677c21073069650b
fisla2 <fisla1146362@mail.wcccd.edu>
2025-04-01 11:26:05 -0400

    Create DebugThree3.java

---
+++ DebugThree3.java
@@ -0,0 +1,10 @@
+public class FixDebugThree3 {
+    public static void main(String[] args) {
+        int num = 5;
+        int result = num / 2;
+        
+        if (result == 2) {  // Corrected to use comparison
+            System.out.println("Result is 2");
+        }
+    }
+}
