bb5caaffa90605b9ee1b3b3eab881899627497a2
fisla2 <fisla1146362@mail.wcccd.edu>
2025-04-01 11:23:42 -0400

    Create  DebugThree1.java

---
+++ DebugThree1.java
@@ -0,0 +1,10 @@
+public class DebugThree1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int result;
+
+        result = num1 + num2;  // Corrected sum
+        System.out.println("The result is: " + result);  // Fixed 'System'
+    }
+}
